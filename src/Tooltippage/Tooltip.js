8642481aa7dac0fa59e2ee5b53957566e5d73699
ankit <ankitchakraborty@30@gmail.com>
2024-08-27 20:04:36 +0530

    remove some errors again

--- src/Tooltippage/Tooltip.js
+++ src/Tooltippage/Tooltip.js
@@ -1,24 +1,22 @@
 import { Tooltip } from 'react-tooltip';
 import 'react-tooltip/dist/react-tooltip.css';
-import styles from './Tooltip.module.css';
+import styles from './Tooltip.module.css'
+// import  {NavLink} from 'react-router-dom'
 
-function TooltipShow({ position }) {
+function TooltipShow({postion}) {
   return (
     <>
-      <div className={styles.component}>
-        <div className={styles.innerComponent}>
-          <a
-            href="#"
-            data-tooltip-id="my-tooltip"
-            data-tooltip-content="Hello, I am a tooltip!"
-            data-tooltip-place={position}
-          >
-            ◕‿‿◕
-          </a>
-        </div>
+    <div className={styles.component}>
+        <div className={styles.innerComponent} >
+      <a  data-tooltip-id="my-tooltip" data-tooltip-content="Hello i am tooltip!" data-tooltip-place={postion}>
+        ◕‿‿◕
+      </a>
+      
       </div>
-
+      </div>
+     
       <Tooltip id="my-tooltip" />
+
     </>
   );
 }
